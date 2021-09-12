d73f44b4604575e69f98f9ad66d8f679c0554904
ofeenee <ofeenee@gmail.com>
2021-09-12 19:48:50 +0300

    write ID.js class

--- ID.js
+++ ID.js
@@ -1,14 +1,49 @@
 import {v4 as uuidv4} from 'uuid';
 import validator from 'validator';
-const {isUUID} = validaotr;
+const {isUUID} = validator;
 
-function ID(id = uuidv4()) {
+function ID() {
   try {
     if (new.target === undefined) return new ID();
+    Object.defineProperties(this, {
+      get: {
+        value: function getID() {
+          try {
+            if (this.id) return this.id;
+            else return null;
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      set: {
+        value: function setID(string = uuidv4()) {
+          try {
+            if (!validateID(string)) throw new Error('ID value is invalid.');
+            else {
+              Object.defineProperty(this, 'id', {
+                value: string,
+                configurable: true
+              });
 
+              return this.id;
+            }
+          } catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      validate: {
+        value: validateID,
+        enumerable: true
+      }
+    });
   }
   catch (error) {
-
+    throw error;
   }
 }
 
@@ -18,8 +53,10 @@ export default ID;
 function validateID(string) {
   try {
     if (typeof string !== 'string' && !string) throw new Error('value is invalid.');
+    if (isUUID(string)) return true;
+    else return false;
   }
   catch (error) {
-
+    throw error;
   }
 }
